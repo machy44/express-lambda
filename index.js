f72cc7bc15fc9b88aa6aa5832bab3b5428a62fc4
alen sljivar <alen.sljivar@miqdigital.com>
2024-12-30 23:11:59 +0100

    create condition for lambda env

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const PORT = 3003;
-
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("home")
-})
+  res.render("home");
+});
 
 app.get("/:id", (req, res) => {
   res.json({
     id: req.params.id
-  })
-})
+  });
+});
+
+if (process.env.ENVIRONMENT === "lambda") {
+  module.exports.handler = serverless(app);
+} else {
+  app.listen(PORT, () => {
+    console.log(`Server has been booted up on port ${PORT}!`);
+  });
+}
